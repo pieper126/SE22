3b80fb58e485c8d35fecfe4dad0571a5da8189ab
pieper126 <stijnbijnen@hotmail.com>
2014-05-29 21:33:29 +0200

    Initial commit

---
+++ SE22/SE22/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SE22.Startup))]
+namespace SE22
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

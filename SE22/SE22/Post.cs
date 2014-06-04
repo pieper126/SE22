20a7c6d7a609f88f52d5b151fd432392fe49315f
pieper126 <stijnbijnen@hotmail.com>
2014-06-04 09:52:48 +0200

    Username added to post

--- SE22/SE22/Post.cs
+++ SE22/SE22/Post.cs
@@ -7,14 +7,17 @@ namespace SE22
 {
     public class Post
     {
-        public Post(int id, string content)
+        public Post(int id, string content, string username)
         {
             this.ID = id;
             this.Content = content;
+            this.Username = username;
         }
 
         public string Content { get; private set; }
 
         public int ID { get; private set; }
+
+        public string Username { get; private set; }
     }
 }

3e9e15b3df96a9a4c6432a7091124da3c0c07f32
pieper126 <stijnbijnen@hotmail.com>
2014-06-05 22:07:50 +0200

    StyleCop likes

--- SE22/SE22/Classes/User.cs
+++ SE22/SE22/Classes/User.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//-----------------------------------------------------------------------
+// <copyright file="User.cs" company="SE22">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace SE22
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    /// <summary>
+    /// Represents a user
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        /// <param name="username">username to identify this user</param>
+        /// <param name="email">email of this user</param>
+        /// <param name="functions">functions of this user</param>
         public User(string username, string email, List<Rights> functions)
         {
             this.Username = username;
@@ -14,10 +29,19 @@ namespace SE22
             this.Functions = functions;
         }
 
+        /// <summary>
+        /// Gets the username used to identify this user
+        /// </summary>
         public string Username { get; private set; }
 
+        /// <summary>
+        /// Gets or sets the email of this user
+        /// </summary>
         public string Email { get; set; }
 
-        public List<Rights> Functions { get; set; }
+        /// <summary>
+        /// Gets the functions of this person possesses 
+        /// </summary>
+        public List<Rights> Functions { get; private set; }
     }
 }

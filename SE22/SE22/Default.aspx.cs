67763cd4fbf008b341a3edfbb1146a9ba9406876
pieper126 <stijnbijnen@hotmail.com>
2014-06-05 14:08:47 +0200

    SubCategory Is now supported

--- SE22/SE22/Default.aspx.cs
+++ SE22/SE22/Default.aspx.cs
@@ -9,52 +9,54 @@ namespace SE22
 {
     public partial class _Default : Page
     {
+        public List<ForumCategory> TopCategorys { get; private set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             MainAdministration.StartUp();
+            TopCategorys = MainAdministration.Categorys.FindAll(x => x.ParentCategory == null);
             initialization();
         }
 
         protected void CategoryButton1_Click(object sender, EventArgs e)
         {
-            Session["NextPage"] = MainAdministration.Categorys[0].ID;
+            Session["NextPage"] = TopCategorys[0];
             Session["PreviousPage"] = "Main";
-            Page.Response.Redirect("Category");
+            Page.Response.Redirect("SubCategory");
         }
 
         protected void CategoryButton2_Click(object sender, EventArgs e)
         {
-            Session["NextPage"] = MainAdministration.Categorys[1].ID;
+            Session["NextPage"] = TopCategorys[1];
             Session["PreviousPage"] = "Main";
-            Page.Response.Redirect("Category");
+            Page.Response.Redirect("SubCategory");
         }
 
         protected void CategoryButton3_Click(object sender, EventArgs e)
         {
-            Session["NextPage"] = MainAdministration.Categorys[2].ID;
+            Session["NextPage"] = TopCategorys[2];
             Session["PreviousPage"] = "Main";
-            Page.Response.Redirect("Category");
+            Page.Response.Redirect("SubCategory");
         }
 
         protected void CategoryButton4_Click(object sender, EventArgs e)
         {
-            Session["NextPage"] = MainAdministration.Categorys[3].ID;
+            Session["NextPage"] = TopCategorys[3];
             Session["PreviousPage"] = "Main";
-            Page.Response.Redirect("Category");
+            Page.Response.Redirect("SubCategory");
         }
 
         private void initialization()
         {
-            if (MainAdministration.Categorys.Count > 3)
+            if (TopCategorys.Count > 3)
             {
-                HyperLink1.Text = MainAdministration.Categorys[0].Name;
-                HyperLink2.Text = MainAdministration.Categorys[1].Name;
-                HyperLink3.Text = MainAdministration.Categorys[2].Name;
-                HyperLink4.Text = MainAdministration.Categorys[3].Name;
-                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[0]).ToString();
-                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[1]).ToString();
-                LblTotalThreads3.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[2]).ToString();
-                LblTotalThreads4.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[3]).ToString();
+                HyperLink1.Text = TopCategorys[0].Name;
+                HyperLink2.Text = TopCategorys[1].Name;
+                HyperLink3.Text = TopCategorys[2].Name;
+                HyperLink4.Text = TopCategorys[3].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[0]).ToString();
+                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[1]).ToString();
+                LblTotalThreads3.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[2]).ToString();
+                LblTotalThreads4.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[3]).ToString();
 
                 HyperLink1.Visible = true;
                 HyperLink2.Visible = true;
@@ -69,14 +71,14 @@ namespace SE22
                 CategoryButton3.Visible = true;
                 CategoryButton4.Visible = true;
             }
-            else if (MainAdministration.Categorys.Count > 2)
+            else if (TopCategorys.Count > 2)
             {
-                HyperLink1.Text = MainAdministration.Categorys[0].Name;
-                HyperLink2.Text = MainAdministration.Categorys[1].Name;
-                HyperLink3.Text = MainAdministration.Categorys[2].Name;
-                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[0]).ToString();
-                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[1]).ToString();
-                LblTotalThreads3.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[2]).ToString();
+                HyperLink1.Text = TopCategorys[0].Name;
+                HyperLink2.Text = TopCategorys[1].Name;
+                HyperLink3.Text = TopCategorys[2].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[0]).ToString();
+                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[1]).ToString();
+                LblTotalThreads3.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[2]).ToString();
 
                 HyperLink1.Visible = true;
                 HyperLink2.Visible = true;
@@ -88,12 +90,12 @@ namespace SE22
                 CategoryButton2.Visible = true;
                 CategoryButton3.Visible = true;
             }
-            else if (MainAdministration.Categorys.Count > 1)
+            else if (TopCategorys.Count > 1)
             {
-                HyperLink1.Text = MainAdministration.Categorys[0].Name;
-                HyperLink2.Text = MainAdministration.Categorys[1].Name;
-                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[0]).ToString();
-                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[1]).ToString();
+                HyperLink1.Text = TopCategorys[0].Name;
+                HyperLink2.Text = TopCategorys[1].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[0]).ToString();
+                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[1]).ToString();
 
                 HyperLink1.Visible = true;
                 HyperLink2.Visible = true;
@@ -102,10 +104,10 @@ namespace SE22
                 CategoryButton1.Visible = true;
                 CategoryButton2.Visible = true;
             }
-            else if (MainAdministration.Categorys.Count > 0)
+            else if (TopCategorys.Count > 0)
             {
-                HyperLink1.Text = MainAdministration.Categorys[0].Name;
-                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(MainAdministration.Categorys[0]).ToString();
+                HyperLink1.Text = TopCategorys[0].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(TopCategorys[0]).ToString();
 
                 HyperLink1.Visible = true;
                 LblTotalThreads1.Visible = true;
---
+++ SE22/SE22/SubCategory.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SE22
+{
+    public partial class SubCategory : System.Web.UI.Page
+    {
+        public List<ForumCategory> Categorys { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["PreviousPage"] != "Main")
+            {
+                Page.Response.Redirect("Default");
+                return;
+            }
+            Categorys = MainAdministration.Categorys.FindAll(x => x.ParentCategory != null);
+            Categorys = Categorys.FindAll(x => x.ParentCategory.ID == ((ForumCategory)Session["NextPage"]).ID);
+            initialization();
+        }
+
+        protected void CategoryButton1_Click(object sender, EventArgs e)
+        {
+            Session["NextPage"] = Categorys[0];
+            Session["PreviousPage"] = "SubCategory";
+            Page.Response.Redirect("Category");
+        }
+
+        protected void CategoryButton2_Click(object sender, EventArgs e)
+        {
+            Session["NextPage"] = Categorys[1];
+            Session["PreviousPage"] = "SubCategory";
+            Page.Response.Redirect("Category");
+        }
+
+        protected void CategoryButton3_Click(object sender, EventArgs e)
+        {
+            Session["NextPage"] = Categorys[2];
+            Session["PreviousPage"] = "SubCategory";
+            Page.Response.Redirect("Category");
+        }
+
+        protected void CategoryButton4_Click(object sender, EventArgs e)
+        {
+            Session["NextPage"] = Categorys[3];
+            Session["PreviousPage"] = "SubCategory";
+            Page.Response.Redirect("Category");
+        }
+
+        private void initialization()
+        {
+            if (Categorys.Count > 3)
+            {
+                HyperLink1.Text = Categorys[0].Name;
+                HyperLink2.Text = Categorys[1].Name;
+                HyperLink3.Text = Categorys[2].Name;
+                HyperLink4.Text = Categorys[3].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[0]).ToString();
+                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[1]).ToString();
+                LblTotalThreads3.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[2]).ToString();
+                LblTotalThreads4.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[3]).ToString();
+
+                HyperLink1.Visible = true;
+                HyperLink2.Visible = true;
+                HyperLink3.Visible = true;
+                HyperLink4.Visible = true;
+                LblTotalThreads1.Visible = true;
+                LblTotalThreads2.Visible = true;
+                LblTotalThreads3.Visible = true;
+                LblTotalThreads4.Visible = true;
+                CategoryButton1.Visible = true;
+                CategoryButton2.Visible = true;
+                CategoryButton3.Visible = true;
+                CategoryButton4.Visible = true;
+            }
+            else if (Categorys.Count > 2)
+            {
+                HyperLink1.Text = Categorys[0].Name;
+                HyperLink2.Text = Categorys[1].Name;
+                HyperLink3.Text = Categorys[2].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[0]).ToString();
+                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[1]).ToString();
+                LblTotalThreads3.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[2]).ToString();
+
+                HyperLink1.Visible = true;
+                HyperLink2.Visible = true;
+                HyperLink3.Visible = true;
+                LblTotalThreads1.Visible = true;
+                LblTotalThreads2.Visible = true;
+                LblTotalThreads3.Visible = true;
+                CategoryButton1.Visible = true;
+                CategoryButton2.Visible = true;
+                CategoryButton3.Visible = true;
+            }
+            else if (Categorys.Count > 1)
+            {
+                HyperLink1.Text = Categorys[0].Name;
+                HyperLink2.Text = Categorys[1].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[0]).ToString();
+                LblTotalThreads2.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[1]).ToString();
+
+                HyperLink1.Visible = true;
+                HyperLink2.Visible = true;
+                LblTotalThreads1.Visible = true;
+                LblTotalThreads2.Visible = true;
+                CategoryButton1.Visible = true;
+                CategoryButton2.Visible = true;
+            }
+            else if (Categorys.Count > 0)
+            {
+                HyperLink1.Text = Categorys[0].Name;
+                LblTotalThreads1.Text = MainAdministration.NumberofThreadsPerCategory(Categorys[0]).ToString();
+
+                HyperLink1.Visible = true;
+                LblTotalThreads1.Visible = true;
+                CategoryButton1.Visible = true;
+            }
+        }
+    }
+}

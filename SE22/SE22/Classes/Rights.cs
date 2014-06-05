a00b4824a14190514024fa054484d305c548c1f9
pieper126 <stijnbijnen@hotmail.com>
2014-06-05 22:14:37 +0200

    StyleCop likes

--- SE22/SE22/Classes/Rights.cs
+++ SE22/SE22/Classes/Rights.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Rights.cs" company="SE22">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace SE22
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    /// <summary>
+    /// All the rights accounts can have
+    /// </summary>
     public enum Rights
     {
+        /// <summary>
+        /// Moderator controls the forum
+        /// </summary>
         Moderator,
+
+        /// <summary>
+        /// Editor is allowed to post articles
+        /// </summary>
         Editor
     }
 }
